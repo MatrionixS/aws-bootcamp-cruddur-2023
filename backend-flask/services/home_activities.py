b7b8f22d0cac0c72a9889cd71dcf8a40d26c5463
Serhii Rusyn <rusinsergey1@gmail.com>
2024-11-27 20:49:35 +0000

    we have query working

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,11 +1,28 @@
 from datetime import datetime, timedelta, timezone
-from lib.db import pool
+from lib.db import pool, query_wrap_array
 class HomeActivities:
   def run():
-    sql = "SELECT * FROM activities;"
+    print("test")
+    query = """
+    SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
+      """
+    sql = query_wrap_array(query)
     with pool.connection() as conn:
       with conn.cursor() as cur:
         cur.execute(sql)
-        json = cur.fetchall()
+        json = cur.fetchone()
         print(json)
-        return json
+        return json[0]
